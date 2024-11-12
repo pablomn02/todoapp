ab5e33a17a55abd85aadc4511a6568cdf289c33b
pablomn02 <pablomnavarro9@gmail.com>
2024-11-12 22:00:50 +0100

    Commit prueba

--- ToDoApp/src/app/task/taskform/taskform.component.ts
+++ ToDoApp/src/app/task/taskform/taskform.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { TaskService } from 'src/app/shared/services/task.service';
 
 @Component({
   selector: 'app-taskform',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./taskform.component.scss']
 })
 export class TaskformComponent {
+
+  constructor(private service: TaskService){}
+
+  newTask = new FormGroup({
+    id: new FormControl( 0, [Validators.required]),
+    name: new FormControl('', [Validators.required]),
+    description: new FormControl('', [Validators.required]),
+    priority: new FormControl('', [Validators.required]),
+    date: new FormControl('', [Validators.required]),
+    completed: new FormControl(false)
+  });
+
+  onSubmit(){
+    if(this.newTask.valid){
+      // this.service.agregarTarea(this.newTask.value)
+      console.log("Nueva tarea añadida: ", this.newTask)
+    }
+  }
 
 }
